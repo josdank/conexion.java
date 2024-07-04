d6c13db5e200c4a0776f6ad1c0a954d3ac68da3d
Safronjos <151254944+josdank@users.noreply.github.com>
2024-07-04 09:42:42 -0500

    Add files via upload

---
+++ conexion con sql.sql
@@ -0,0 +1,18 @@
+create database estudiantes;
+
+use estudiantes;
+
+insert into estudiantes(cedula,nombre,BIM_1,BIM_2)
+values
+('1752370344','Josue Guerra',18.4,16.5),
+('1596842738','Adrian Ramos',18.6,15.4),
+('1554782496','Carlos Pérez',15.65,16.80),
+('1864328795','Richard Soria',16.25,14.32),
+('1754824968','Santiago Cumbal',15.24,16.32),
+('1596842746','Joel Parra',17.25,16.42),
+('1568249375','Mireya Garcia',16.40,18.52),
+('1726395709','Adrian Cadena', 13.45, 10.76),
+('1736492203','Paul Cabrera', 15.67, 16.45),
+('1826152241','Mireya Garcia', 17.89, 18.12);
+
+select * from estudiantes;
